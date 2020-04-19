294118b34a472bad5406598270796fa6d83427cf
Pawel Haladyj <pawel@haladyj.pl>
2020-04-19 10:28:53 +0200

    initial commit

---
+++ src/test/java/pl/haladyj/springsecurity9/SpringSecurity9ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
